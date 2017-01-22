e3ae180a512a8c037d893fa127a2ca43ccad6af7
Gabriel Calvo <gabo.calvo1@gmail.com>
2017-01-22 14:08:22 -0500

    Creación de constantes para la generación de respuestas

--- src/main/java/com/cloud/spring/pruebaGrupoASD/util/comun/ConstanteUtil.java
+++ src/main/java/com/cloud/spring/pruebaGrupoASD/util/comun/ConstanteUtil.java
@@ -17,13 +17,16 @@ public class ConstanteUtil {
      *
      * CODIGOS DE RESPUESTA
      */
-    public static final int CODE_OK = HttpStatus.OK.value();
-    public static final int CODE_ERROR = HttpStatus.CONFLICT.value();
-    public static final int CODE_INTERNAL_ERROR = HttpStatus.INTERNAL_SERVER_ERROR.value();
+    
+    public static final int CODE_OK = 200;
+    public static final int CODE_FALTANTES= 400;
+    public static final int CODE_ERROR = 404;
+    public static final int CODE_INTERNAL_ERROR = 500;
 
     public static final String MSG_REGISTRO_EXITOSO = "Tu registro ha sido agregado.";
     public static final String MSG_UPDATE_EXITOSO = " Tu registro ha sido modificado.";
 
   
+    public static final String FORMATO_FECHA = "dd/MM/yyyy";
     public static final String FORMATO_FECHA_CON_HORA = "dd/MM/yyyy hh:mm:ss a";
 }
