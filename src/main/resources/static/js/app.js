e0390fcf43ed778e022ff7443081488d1cad5b32
Gabriel Calvo <gabo.calvo1@gmail.com>
2017-01-22 21:57:33 -0500

    cliente angular para consumir servicio rest

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var app = angular.module('app', ['ngRoute', 'ngResource']);
+app.config(function ($routeProvider) {
+    $routeProvider
+            .when('/activos', {
+                templateUrl: '/view/activofijos.html'
+            })
+            .when('/gestionar-activos', {
+                templateUrl: '/view/gestionar-activos-fijos.html'
+            })
+            .otherwise(
+                    {redirectTo: '/'}
+            );
+});
